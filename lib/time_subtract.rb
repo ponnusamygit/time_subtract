7fc4ddf97295223c96ce60dffeddb096b2a8a98a
Ponnusamy <ponnusamy.gac@gmail.com>
2014-06-27 09:39:08 +0530

    Init commit

---
+++ lib/time_subtract.rb
@@ -0,0 +1,10 @@
+require "time_subtract/version"
+require "time_subtract/subtract_hash"
+
+module TimeSubtract
+	def smart_subtract time
+
+	end
+end
+
+Time.send :include, TimeSubtract
---
+++ lib/time_subtract/version.rb
@@ -0,0 +1,3 @@
+module TimeSubtract
+  VERSION = "0.0.1"
+end
